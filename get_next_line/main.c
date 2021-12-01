a0d6cd97fd137477467884f6d1898e7c3db2c070
gchatain <guilheimchataing@gmail.com>
2021-12-01 19:13:16 +0100

    bordel

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -2,13 +2,16 @@
 
 int main(void)
 {
+	setbuf(stdout, NULL);
 	int fd = open("test.txt",'r');
 	char *str;
+	int	limit;
 
 	str = get_next_line(fd);
+	limit = 10;
 	while (str)
 	{
-		printf("%s\n",str);
+		printf("%s\n\n",str);
 		free(str);
 		str = get_next_line(fd);
 	}
