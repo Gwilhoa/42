81f5aab88d18c2c5a1736b1bf0288bba1534b7e6
Gwilhoa <guilheimchataing@gmail.com>
2021-12-10 15:15:47 +0100

    fin ?

--- get_next_line/get_next_line_bonus.c
+++ get_next_line/get_next_line_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gchatain <gchatain@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gchatain <gchatain@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 16:25:15 by gchatain          #+#    #+#             */
-/*   Updated: 2021/12/08 18:28:48 by gchatain         ###   ########lyon.fr   */
+/*   Updated: 2021/11/18 16:25:15 by gchatain         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,69 +26,82 @@ int	ft_search(char *str, int charset)
 	return (-1);
 }
 
-char	*ft_has_nl(char *ret, char **reste)
+char	*ft_has_nl(char *ret, char *rest)
 {
 	char	*temp;
+	int		i;
+	int		n;
 
-	temp = ft_strdup(ret + ft_search(ret, '\n') + 1);
-	if (ft_strlen(temp) != 0)
-		*reste = ft_strdup(temp);
+	n = ft_search(ret, '\n');
+	i = 0;
+	temp = ft_strdup(ret + n + 1);
+	while (temp[i] != 0)
+	{
+		rest[i] = temp[i];
+		i++;
+	}
 	free(temp);
-	ret[ft_search(ret, '\n') + 1] = 0;
+	rest[i] = 0;
+	ret[n + 1] = 0;
 	return (ret);
 }
 
-char	*ft_init(char *ret, char *str)
+char	*ft_init(char *ret, char *str, int r)
 {
 	char	*temp;
 
+	str[r] = '\0';
 	if (!ret)
 	{
 		ret = malloc(1);
 		ret[0] = 0;
 	}
 	temp = ft_strjoin(ret, str);
+	if (!temp)
+		return (0);
 	free(ret);
 	free(str);
 	return (temp);
 }
 
-int	ft_reader(char **str, int fd)
+int	reader(char **str, int fd)
 {
-	char	*temp;
+	char	*ret;
 	int		r;
 
-	temp = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	r = read(fd, temp, BUFFER_SIZE);
-	*str = temp;
+	ret = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	r = read(fd, ret, BUFFER_SIZE);
+	*str = ret;
+	if (r <= 0)
+		free(*str);
 	return (r);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*reste[OPEN_MAX];
+	static char	rest[10240][BUFFER_SIZE + 1];
 	char		*str;
 	char		*ret;
 	int			r;
 
 	ret = 0;
-	if (fd >= 0 && reste[fd])
+	if (fd >= 0 && *rest[fd])
 	{
-		ret = ft_strdup(reste[fd]);
-		free(reste[fd]);
-		reste[fd] = NULL;
+		ret = ft_strdup(rest[fd]);
+		*rest[fd] = 0;
 	}
-	r = ft_reader(&str, fd);
+	r = 1;
 	while (r > 0 || (ret && ft_search(ret, '\n') != -1))
 	{
-		if (r < 0)
-			return (0);
-		str[r] = '\0';
-		ret = ft_init(ret, str);
-		if (ft_search(ret, '\n') != -1)
-			return (ft_has_nl(ret, &reste[fd]));
-		r = ft_reader(&str, fd);
+		if (!ret || ft_search(ret, '\n') == -1)
+		{
+			r = reader(&str, fd);
+			if (r <= 0)
+				return (ret);
+			ret = ft_init(ret, str, r);
+		}
+		if (ret && ft_search(ret, '\n') != -1)
+			return (ft_has_nl(ret, rest[fd]));
 	}
-	free(str);
-	return (ret);
+	return (0);
 }
--- get_next_line/get_next_line_bonus.h
+++ get_next_line/get_next_line_bonus.h
@@ -1,19 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gchatain <gchatain@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gchatain <gchatain@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 16:47:33 by gchatain          #+#    #+#             */
-/*   Updated: 2021/12/02 19:33:36 by gchatain         ###   ########lyon.fr   */
+/*   Updated: 2021/11/18 16:47:33 by gchatain         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
 
-# include <limits.h>
 # include <stdlib.h>
 # include <stdio.h>
 # include <strings.h>
@@ -28,7 +27,9 @@ char	*ft_strjoin(char const *s1, char const *s2);
 int		ft_strlen(char const *str);
 char	*ft_strdup(char const *src);
 char	*get_next_line(int fd);
-char	*ft_has_nl(char *ret, char **reste);
-char	*ft_init(char *ret, char *str);
+char	*ft_has_nl(char *ret, char *reste);
+char	*ft_init(char *ret, char *str, int r);
+void	ft_cleaner(char *reste);
+int		reader(char **str, int fd);
 
 #endif
