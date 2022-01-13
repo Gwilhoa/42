9a244d821e35145ddac27dd4f861bd20b057a9d6
gchatain <gchatain@student.42.fr>
2022-01-13 13:56:12 +0100

    iugrds

--- pushswap/main.c
+++ pushswap/main.c
@@ -6,29 +6,41 @@
 /*   By: gchatain <gchatain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 15:18:59 by gchatain          #+#    #+#             */
-/*   Updated: 2022/01/11 21:46:32 by gchatain         ###   ########lyon.fr   */
+/*   Updated: 2022/01/13 13:49:43 by gchatain         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int main(void)
+int main(int argc, char const *argv[])
 {
 	t_list	*stacka;
 	t_list	*stackb;
 
 	stacka = NULL;
 	stackb = NULL;
-	lst_add_front(&stacka, 1);
-	lst_add_front(&stacka, 2);
-	lst_add_front(&stacka, 3);
-	lst_add_front(&stacka, 4);
-	lst_add_front(&stacka, 5);
-	lst_add_front(&stacka, 6);
-	lst_add_front(&stacka, 7);
-	lst_add_front(&stacka, 8);
-	lst_add_front(&stacka, 9);
-	reverse_rotate(&stacka);
-	lst_display(stacka);
-	return 0;
+	if (ft_complete_args(argv, &stacka) == 0)
+		ft_printf("\n");
+	return (argc);
+}
+
+int	ft_complete_args(char const *argv[], t_list **lst)
+{
+	int	i;
+	int	j;
+	int	temp;
+
+	i = 1;
+	while (argv[i])
+	{
+		while (argv[i][j])
+		{
+			if (ft_isdigit(argv[i][j]))
+			j++;
+		}
+		
+		temp = ft_atoi(argv[i]);
+		i++;
+	}
+	
 }
