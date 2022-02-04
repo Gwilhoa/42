9bf1aa6e3715ec0836097f3556e12728e8d2ae07
Gwilhoa <guilheimchataing@gmail.com>
2022-02-04 11:41:39 +0100

    '(ztzeterze

--- so_long/includes/so_long.h
+++ so_long/includes/so_long.h
@@ -3,13 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gchatain <gchatain@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gwilhoa <gwilhoa@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/02 12:41:29 by gchatain          #+#    #+#             */
-/*   Updated: 2022/02/02 13:49:27 by gchatain         ###   ########lyon.fr   */
+/*   Updated: 2022/02/04 11:27:08 by gwilhoa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "mlx.h"
-#include "get_next_line_bonus.h"
-#include "libft.h"
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include "mlx.h"
+# include "get_next_line_bonus.h"
+# include "libft.h"
+
+typedef struct s_long
+{
+	int		width;
+	int		height;
+	STRING	**matrice;
+}	t_long;
+
+#endif
--- so_long/sources/main.c
+++ so_long/sources/main.c
@@ -6,26 +6,73 @@
 /*   By: gwilhoa <gwilhoa@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 12:35:15 by gchatain          #+#    #+#             */
-/*   Updated: 2022/02/03 22:22:00 by gwilhoa          ###   ########.fr       */
+/*   Updated: 2022/02/04 11:40:26 by gwilhoa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	main(void)
+int	main(int argc, char const *argv[])
 {
 	void	*link;
 	void	*fen;
-	void	*wall;
-	int		size;
 	int		fd;
+	t_long	*game;
 
-	size = 16;
-	fd = open("test.ber", O_RDONLY);
+	if (init(argc, argv, &game) == 0)
+	{
+		ft_putstr_fd("Error\n map format", 1);
+		exit(EXIT_FAILURE);
+		return (0);
+	}
 	link = mlx_init();
-	fen = mlx_new_window(link, 500, 500, "test");
-	wall = mlx_xpm_file_to_image(link, "sprites/wall.xpm", &size, &size);
-	mlx_put_image_to_window(link, fen, wall, 0, 0);
+	fen = mlx_new_window(link, game->height, game->width, "so_long");
 	mlx_loop(link);
 	return (0);
+}
+
+int	init(int argc, char const *argv[], t_long **game)
+{
+	int		fd;
+	int		*ret;
+	STRING	str;
+	t_long	*temp;
+
+	temp = malloc(1 * sizeof(t_long));
+	if (argc != 2)
+		return (0);
+	fd = open(argv[1], O_RDONLY);
+	str = get_next_line(fd);
+	if (!str)
+		return (0);
+	temp->height = ft_strlen(str);
+	if (complete_matrice(fd, &temp, str) == 0)
+	{
+		game = &temp;
+		return (0);
+	}
+	game = &temp;
+	return (1);
+}
+
+int	complete_matrice(int fd, t_long **game, STRING str)
+{
+	t_long	*temp;
+	int		i;
+
+	i = 0;
+	temp = *game;
+	temp->matrice = malloc((1 + temp->height) * sizeof(STRING *));
+	while (str)
+	{
+		temp->matrice[i] = str;
+		str = get_next_line(str);
+		i++;
+	}
+	temp->matrice[i] = NULL;
+	temp->width = i;
+	game = &temp;
+	if (temp->height < temp->width)
+		return (0);
+	return (1);
 }
