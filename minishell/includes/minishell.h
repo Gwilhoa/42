c0f42382803f299cb1ef2ba68fe6733ebee3080f
gchatain <gchatain@student.42.fr>
2022-04-04 15:12:21 +0200

    test

--- minishell/includes/minishell.h
+++ minishell/includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: gchatain <gchatain@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 10:05:50 by gchatain          #+#    #+#             */
-/*   Updated: 2022/03/22 14:30:28 by gchatain         ###   ########lyon.fr   */
+/*   Updated: 2022/04/04 11:32:53 by gchatain         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,18 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 
-int	getcmd(char **args);
-int	ft_echo(char **args);
-int	ft_pwd(void);
+typedef struct s_cmd
+{
+	STRING		redirecting_tag;
+	STRING		redirecting;
+	STRING		cmd;
+	char		**args;
+}	t_cmd;
+
+int		ft_echo(char **args);
+int		main(void);
+int		getcmd(char **args);
+int		ft_pwd(void);
+int		ft_isredirecting(const STRING str);
 
 #endif
--- minishell/sources/readline.c
+++ minishell/sources/readline.c
@@ -6,7 +6,7 @@
 /*   By: gchatain <gchatain@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 09:23:47 by gchatain          #+#    #+#             */
-/*   Updated: 2022/03/22 14:30:43 by gchatain         ###   ########lyon.fr   */
+/*   Updated: 2022/04/04 11:33:21 by gchatain         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,19 +30,34 @@ int	main(void)
 			add_history(line);
 			args = ft_split(line, ' ');
 			if (getcmd(args) == 0)
-				ft_printf("%sminishell: %s:command not found\n%s", RED, line, WHITE);
+			{
+				ft_printf("%sminishell: %s", RED, line);
+				ft_printf("command not found\n%s", WHITE);
+			}
 		}
 	}
 	return (0);
 }
 
+/**
+ * @brief program's center
+ * 
+ * @param args argument's command
+ * @return int (return 0 if the command does'nt exist)
+ */
 int	getcmd(char **args)
 {
-	if (ft_strncmp(args[0], "pwd", ft_strlen(args[0])) == 0)
-		return (ft_pwd());
+	t_cmd	cmd;
+
+	cmd = ft_init(args);
 	return (0);
 }
 
+/**
+ * @brief function bash pwd
+ * 
+ * @return int (1 if success)
+ */
 int	ft_pwd(void)
 {
 	char	dir[256];
@@ -50,4 +65,49 @@ int	ft_pwd(void)
 	getcwd(dir, sizeof(dir));
 	ft_printf("%s\n", dir);
 	return (1);
+}
+
+/**
+ * @brief function to know if the string is a redirecting bash
+ * 
+ * @param str (at comparate)
+ * @return int (1 is redirecting)
+ */
+int	ft_isredirecting(const STRING str)
+{
+	if (ft_strncmp(str, ">", ft_strlen(str)) == 0)
+		return (1);
+	if (ft_strncmp(str, ">>", ft_strlen(str)) == 0)
+		return (1);
+	if (ft_strncmp(str, "<", ft_strlen(str)) == 0)
+		return (1);
+	if (ft_strncmp(str, "<<", ft_strlen(str)) == 0)
+		return (1);
+	return (0);
+}
+
+/**
+ * @brief initalization of struct cmd (parsing)
+ * if the bonuses are to be done, modify the structure in structure list
+ * @param args 
+ * @return t_cmd 
+ */
+t_cmd	ft_init(char	**args)
+{
+	t_cmd	cmd;
+	int		i;
+
+	if (ft_isredirecting(args[0]) == 1)
+	{
+		cmd.redirecting = args[1]);
+		cmd.redirecting_tag = args[0];
+		cmd.cmd = args[2];
+		cmd.args = args;//
+	}
+	else
+	{
+		cmd.redirecting = 1;//
+		cmd.cmd = args[0];
+		cmd.args = args;
+	}
 }
